c107cc3c84f96b6da794696748c84dff82ab0cb2
Abdiel Batista dos Santos <abdielb@yahoo.com>
2021-12-01 18:34:13 -0300

    route initial

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const dotenv = require('dotenv');
 
 const app = express();
 
+//ler JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//rota
+//const encripRoutes = require('./routes/encripRoutes');
+//app.use('/encrip', encripRoutes);
+
+//rota inicial
+app.get('/',(req,res)=>{
+    res.json({message: "Olá humano"});
+})
+
+app.listen(3000)
+
+/*.then(()=>{
+    console.log('Conectado')
+    app.listen(3000)
+}).catch((err)=> console.log(err));*/
---
+++ routes/encripRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const {aplication} = require('express');
+const Encrip = require('../models/Encrip');
+
+
+module.exports = router
